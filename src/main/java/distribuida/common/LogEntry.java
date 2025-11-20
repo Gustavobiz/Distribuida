15708a1b62c5067fe9006e75ba4651271a7c5dd3
Gustavobiz <gustavobernardes05@hotmail.com>
2025-11-20 00:59:36 -0300

    logentry

---
+++ src/main/java/distribuida/common/LogEntry.java
@@ -0,0 +1,27 @@
+package main.java.distribuida.common;
+
+public class LogEntry {
+    public int index;
+    public int term;
+    public String key;
+    public String value;
+
+    public LogEntry() {}
+
+    public LogEntry(int index, int term, String key, String value) {
+        this.index = index;
+        this.term = term;
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "LogEntry{" +
+                "index=" + index +
+                ", term=" + term +
+                ", key='" + key + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
